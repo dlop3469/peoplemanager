e0254ac1367aa9a1de60d728f501ff77344b46d2
David Lopez Padilla <dlop3469@gmail.com>
2014-08-01 20:21:27 +0100

    Create Test
      - Create package structure in test
      - Create a first test class
      - Start defining tests to fulfill the requirements

---
+++ src/test/java/com/examples/peoplemanager/PeopleManagerTest.java
@@ -0,0 +1,9 @@
+package com.examples.peoplemanager;
+
+/**
+ * 1.- How many males are in the address book
+ * 2.- Who is the oldest person in the address book?
+ * 3.- How many days older is Bill than Paul?
+ */
+public class PeopleManagerTest {
+}
