1dd9e5fd1cb5aee227f8c43a99dc7260652372f1
David Lopez Padilla <dlop3469@gmail.com>
2014-08-01 20:23:44 +0100

    First test
      - For the first requirement, check if number of people is empty.
      - At this point, test is not compiling.
      - The implementation needs to be added to make the test pass.

--- src/test/java/com/examples/peoplemanager/PeopleManagerTest.java
+++ src/test/java/com/examples/peoplemanager/PeopleManagerTest.java
@@ -1,9 +1,20 @@
 package com.examples.peoplemanager;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * 1.- How many males are in the address book
  * 2.- Who is the oldest person in the address book?
  * 3.- How many days older is Bill than Paul?
  */
+
 public class PeopleManagerTest {
+
+    @Test
+    public final void whenRequestNumberOfMalesOnEmptyPeopleReturnsNone() {
+        PeopleManager pm = new Peoplemanager();
+
+        Assert.assertEquals(pm.getNumberOfMales(), 0);
+    }
 }
